a90d578e03f2ce51c6bc147ead14bddc7a5ea920
Gustavo Panda <gus.contreras.m@gmail.com>
2020-12-29 21:48:06 -0600

    Actualización 29 de diciembre de 2020 - Mac
    Se actualizan los archivos del Tema 5.

--- Tema 5 - Funciones Especiales/Codigos python/cadena_oscilante.py
+++ Tema 5 - Funciones Especiales/Codigos python/cadena_oscilante.py
@@ -9,20 +9,25 @@ import numpy as np
 from scipy.special import j0, jn_zeros
 import matplotlib.pyplot as plt
 from matplotlib import animation
-#from IPython.display import HTML
+plt.rcParams['animation.ffmpeg_path'] = '/usr/local/bin/ffmpeg'
 
 # Aceleracion debida a la gravedad s^-2
 g = 9.81
+
 # Longitud de la cadena, m
 L = 1
+
 # Amplitud máxima, m
 A = 0.05
+
 # Eje vertical (m)
 z = np.linspace(0, L, 201)
+
 # Eje vertical escalado
 u = 2 * np.sqrt(z/g)
 
-mode = 2
+mode = 1
+
 # jn_zeros calcula los primeros n ceros de Jo(x)
 w = jn_zeros(0, mode)[mode-1] * np.sqrt(g/L) / 2
 
@@ -33,34 +38,42 @@ fig, ax = plt.subplots(figsize=(4,6))
 ax.axis('off')
 ax.set_xlim((-2*A, 2*A))
 ax.set_ylim((0, L))
-ax.set_title('m = {}'.format(mode))
+ax.set_title('Oscilación con m = {}'.format(mode))
 
-line, = ax.plot([], [], 'k', dashes=[5,2], lw=3)
-
+line, = ax.plot([], [], 'k', dashes=[5, 2], lw=3)
 
 # Da inicio a la funcion para la animacion
 def init():
     line.set_data([], [])
-    return (line,)
+    return line,
 
 
 # Retraso entre los frames (ms)
 interval = 10
+
 # Total de frames en la animacion para tener un periodo de oscilacion completo
 nframes = int(2 * np.pi / w * 1000 / interval)
+
 
 # Llamada secuencial para la funcion de animacion
 def animate(i):
     t = i * interval / 1000
     x = A * j0(w*u) * np.cos(w*t)
     line.set_data(x, z)
-    return (line,)
+    return line,
 
 
 # Configuracion de la animacion
-anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=nframes, interval=interval, blit=True)
+anim = animation.FuncAnimation(fig, animate, frames=10*nframes, init_func=init, interval=interval)
+
+writer = animation.writers['ffmpeg'](fps=30)
+
 
 
 # Se guarda la animacion en un archivo gif
-anim.save('chain_{}.gif'.format(mode), writer='imagemagick', fps=1000/interval)
+
+#anim.save('cadena_{}.gif'.format(mode), writer='imagemagick', fps=1000/interval)
+
+# Se guarda la animacion en un archivo mp4
+
+anim.save('cadena_m{}.mp4'.format(mode), writer=writer, dpi=100)
