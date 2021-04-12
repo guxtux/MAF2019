c62e3392d346bc956c5e9395bf5ec4ad9bfe8019
guxtux <gus.contreras.m@gmail.com>
2021-04-12 17:31:50 -0500

    Actualización 12 de abril 2021 - Laptop
    Se completan las presentaciones con ejercicios de las funciones Gamma y Beta del Tema 1.

--- Tema 4 - Funcion Gamma y Beta/Codigo pyhon/plot_estrella.py
+++ Tema 4 - Funcion Gamma y Beta/Codigo pyhon/plot_estrella.py
@@ -10,8 +10,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 a = 1
-b = 2.
-c = 3.
+b = 20.
+c = 1.
 t = np.linspace(-1, 1, 500)
 
 def y1(x):
@@ -28,7 +28,7 @@ def todosCuadrantes(t):
     plt.plot(t, y2(t), color='k')
     plt.plot(-y1(t), t, color='k')
     plt.plot(y2(t), -t, color='k')
-    plt.title('Curva $x^{b/c} + y^{b/c} = a^{b/c}$ donde $b < c$')
+    plt.title('Curva $x^{20} + y^{20} = a^{20}$')
     
 def ejesGrafica():
     plt.axhline(y=0, lw=0.8, color='k')
@@ -40,7 +40,7 @@ def ejesGrafica():
 def cuadranteGrafica():
     primerCuadrante
     plt.fill_between(t, 0, y1(t), alpha=0.5)
-    plt.title('Primer cuadrante para la curva $x^{b/c} + y^{b/c} = a^{b/c}$ donde $b < c$')
+    plt.title('Primer cuadrante para la curva $x^{20} + y^{20} = a^{20}$')
     plt.text(0.15, 0.25, "R", fontsize="20")
 
 plt.figure(1)
